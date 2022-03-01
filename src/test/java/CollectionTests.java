71e97c5c85753845613522f394e85990a7046d79
dujianwei.meng <dujianwei.meng@bytedance.com>
2022-03-01 15:04:15 +0800

    feat(collection): 测试集合相关api

--- src/test/java/CollectionTests.java
+++ src/test/java/CollectionTests.java
@@ -1,32 +1,71 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+@Slf4j
 public class CollectionTests {
 
     @Test
-    public void method1() {
+    public void testListSort() {
 
-        Map<String, String> map = new HashMap<>();
-        map.put("k-1", "v-1");
-        map.put("k-2", "v-2");
-        map.put("k-3", "v-3");
+        List<Integer> list = IntStream.range(0, 100)
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
 
-        List<String> list = new ArrayList<>(map.values());
+        log.info("before sort, orderedList: {}", list);
+        log.info("-------------------------");
 
-        System.out.println("map: " + map);
-        System.out.println("list: " + list);
-        System.out.println("------");
+        Comparator<Integer> comparator = (a, b) -> b - a;
 
-        map.clear();
-        System.out.println("map: " + map);
-        System.out.println("list: " + list);
+        List<Integer> orderedList = list.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
 
+        log.info("after sort, orderedList: {}", orderedList);
+        log.info("-------------------------");
+
+        int index = Collections.binarySearch(orderedList, 15, comparator);
+        log.info("index: [{}], key: [{}]", index, orderedList.get(index));
+
+        Assertions.assertEquals(0, Collections.binarySearch(orderedList, 99, comparator));
+        Assertions.assertEquals(1, Collections.binarySearch(orderedList, 98, comparator));
+        Assertions.assertEquals(84, Collections.binarySearch(orderedList, 15, comparator));
+        Assertions.assertEquals(15, Collections.binarySearch(orderedList, 84, comparator));
+        Assertions.assertEquals(99, Collections.binarySearch(orderedList, 0, comparator));
     }
 
+    @Test
+    public void testFlatMap() {
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("dubbo");
+        list1.add("spring cloud");
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("java");
+        list2.add("golang");
+        list2.add("python");
+
+        List<String> list3 = new ArrayList<>();
+        list3.add("js");
+        list3.add("es");
+        list3.add("ts");
+
+        List<List<String>> lists = Stream.of(list1, list2, list3)
+                .collect(Collectors.toList());
+
+        log.info("lists: {}", lists);
+
+        List<String> collect = lists.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        log.info("collect: {}", collect);
+    }
 
 
 }
