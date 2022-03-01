d6db0222a93543a7c185603058b7a3cc5fe910ce
dujianwei.meng <dujianwei.meng@bytedance.com>
2022-03-01 18:34:26 +0800

    refactor(executor): 重构串行化线程池的单元测试方法

--- src/test/java/SerializingExecutorTest.java
+++ src/test/java/SerializingExecutorTest.java
@@ -1,50 +1,66 @@
 import com.rice.meng.common.threadpool.SerializingExecutor;
+import com.rice.meng.common.threadpool.ThreadHelper;
+import com.rice.meng.common.util.RandomUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
+/**
+ * https://mp.weixin.qq.com/s/OKleJdGzbv6z3xexKCG-pA
+ */
+@Slf4j
 public class SerializingExecutorTest {
 
     protected static SerializingExecutor serializingExecutor;
 
     @BeforeAll
     public static void before() {
-        ExecutorService service = Executors.newFixedThreadPool(4);
+        ExecutorService service = ThreadHelper.createFixedThreadPool(10, 100, "serializing");
         serializingExecutor = new SerializingExecutor(service);
     }
 
     @Test
     public void test1() throws InterruptedException {
         int n = 2;
-        int eachCount = 1000;
+        int eachCount = 50;
         int total = n * eachCount;
-        int sleepMillis = 10;
+        int sleepTime = 10;
+        String key = "key";
 
         Map<String, Integer> map = new HashMap<>();
-        map.put("val", 0);
+        map.put(key, 0);
 
         CountDownLatch downLatch = new CountDownLatch(total);
 
-        for (int i = 0; i < total; i++) {
-            final int index = i;
-            Thread.sleep(ThreadLocalRandom.current().nextInt(sleepMillis));
-            serializingExecutor.execute(() -> {
-                try {
-                    Thread.sleep(ThreadLocalRandom.current().nextInt(sleepMillis));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                int num = map.get("val");
-                map.put("val", num + 1);
-                downLatch.countDown();
-                Assertions.assertEquals(num, index);
-            });
+        IntStream.range(0, total)
+                .forEach(i -> {
+                    // sleep(sleepTime);
+                    serializingExecutor.execute(() -> {
+                        // sleep(sleepTime);
+                        int num = map.get(key);
+                        log.info("value: [{}]", num);
+                        map.put(key, num + 1);
+                        downLatch.countDown();
+                        Assertions.assertEquals(num, i);
+                    });
+                });
+        downLatch.await();
+        Assertions.assertEquals(total, map.get(key));
+    }
+
+    private void sleep(int sleepMillis) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(RandomUtils.nextInt(0, sleepMillis));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        downLatch.await(3, TimeUnit.SECONDS);
-        Assertions.assertEquals(total, map.get("val"));
     }
 }
